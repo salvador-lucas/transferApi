39262ee3abb0217931ce396d8b9c0e99f26ed7c7
lucas salvador <54772470+salvador-lucas@users.noreply.github.com>
2023-04-05 14:29:23 -0300

    removed unused vars

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,13 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { login, userSignup } from '~api/services/auth';
 import { STATUS_CODES } from '~constants';
 
-export const handleUserSignup = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+export const handleUserSignup = async (req: Request, res: Response): Promise<void> => {
   await userSignup(req.body);
   res.status(STATUS_CODES.CREATED).send();
 };
 
-export const handleLogin = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+export const handleLogin = async (req: Request, res: Response): Promise<void> => {
   await login(req.body);
   res.status(STATUS_CODES.CREATED).send();
 };
--- src/api/controllers/transfers.ts
+++ src/api/controllers/transfers.ts
@@ -1,13 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { createTransfer, getTransfers } from '~api/services/transfer';
 import { STATUS_CODES } from '~constants';
 
-export const handleCreateTransfer = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+export const handleCreateTransfer = async (req: Request, res: Response): Promise<void> => {
   await createTransfer(req.body);
   res.status(STATUS_CODES.CREATED).send();
 };
 
-export const handleGetTransfer = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
+export const handleGetTransfer = async (req: Request, res: Response): Promise<void> => {
   const transfers = await getTransfers(req.query);
   res.status(STATUS_CODES.OK).send(transfers);
 };
